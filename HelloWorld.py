c95f7bac396b3017363c137ecdd76bb95b021b75
Leo.OY <372889971@qq.com>
2014-11-30 01:01:42 +0800

    french to French

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,4 +4,4 @@ Created on 2014-11-30
 @author: ouyangsiqi
 '''
 
-print "Hello World! My English name is Michael Jack, my french name is Helodie Jaqueline!"
+print "Hello World! My English name is Michael Jack, my French name is Helodie Jaqueline!"
